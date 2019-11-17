fc4a34a0538b687ea488c0e4999117c1735ec3b9
Vitalii Kukhar <kukhar.vitalii@gmail.com>
2019-11-17 18:36:05 +0200

    add hash lib

--- src/init.js
+++ src/init.js
@@ -4,12 +4,13 @@ import fingerprint from './modules/fingerprint2';
 import async from './util/async';
 import render from './modules/render';
 
-export default () => {
-    (async () => {
-        const hash = await async(fingerprint);
-        if (hash) {
-            render.on();
-        }
-        console.log(hash);
-    })();
+export default {
+    start: () =>
+        (async () => {
+            const hash = await async(fingerprint);
+            if (hash) {
+                render.on();
+            }
+            console.log(hash);
+        })()
 };
--- src/util/hash.js
+++ src/util/hash.js
@@ -1,7 +1,24 @@
 'use strict';
 
+import createHash from 'create-hash';
+
 export default (fingerprints, config) => {
-    const result = '';
-    
-    return result;
+    const hash = createHash('sha224');
+    const encrypted = [];
+
+    hash.write('ksjdhskjdfhksjdfhksjdhfkjsdhfjskdfhkjsdhfkjsdhfkjsdhfjskdhfkjsdhfkjsdhfkjsdhfjksdhfjksdhfjskdfhksjdfhsdjkf');
+
+
+    hash.on('readable', () => {
+        let chunk;
+        while (null !== (chunk = hash.read())) {
+            encrypted.push(chunk.toString('hex'));
+          }
+    });
+
+    hash.on('end', () => {
+        console.log(encrypted.join());
+    });
+
+    hash.end();
 };
