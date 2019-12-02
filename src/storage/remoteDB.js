b4a3d598bf537998c415daaf7badbc90da92e1cd
Vitalii Kukhar <kukhar.vitalii@gmail.com>
2019-12-02 23:46:20 +0200

    rewritten remoteDB

--- src/storage/remoteDB.js
+++ src/storage/remoteDB.js
@@ -1,30 +1,27 @@
 'use strict';
 
 import advCo from '../config/advertising';
+import dbwrapper from '../util/dbwrapper';
 import '../util/jquery';
 
 const { url } = advCo;
 
 export default {
     get: key => {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: `${url}?key=${key}`,
-                type: 'GET'
-            })
-            .done(result => resolve(result))
-            .fail(err => reject(err));
+        return dbwrapper(() => {
+            return $.ajax({
+                        url: `${url}?key=${key}`,
+                        type: 'GET'
+                    });
         });
     },
     set: (key, data) => {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url,
-                type: 'POST',
-                data: `key=${key}&data=${JSON.stringify(data)}`,
-            })
-            .done(result => resolve(result))
-            .fail(err => reject(err));
+        return dbwrapper(() => {
+            return $.ajax({
+                        url,
+                        type: 'POST',
+                        data: `key=${key}&data=${JSON.stringify(data)}`,
+                    });
         });
     }
 };
